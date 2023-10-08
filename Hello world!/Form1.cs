0caf6b439b5537cb876ef08f1327d1c99ed410f9
User <User@WIN-G79SF1H2H9Q>
2023-10-08 15:55:59 +0300

    Добавьте файлы проекта.

---
+++ Hello world!/Form1.cs
@@ -0,0 +1,27 @@
+namespace Hello_world_
+{
+    public partial class Form1 : Form
+    {
+        public object HelloWorld { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello World!";
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "???";
+        }
+    }
+}
